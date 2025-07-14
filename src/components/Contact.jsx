fa208f295ac8e7770d3590332fedae09dc4e9bf4
bhargavasai <cherukuribhargavsai@gmail.com>
2025-07-14 15:50:57 +0530

    contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <h2>Contact Me</h2>
       <p>Email:<a href="mailto:cherukuribhargav20@gmail.com" target="_blank">Send me an email</a><br></br></p>
       <p>GitHub: <a href="https://github.com/Bhargava2188" target="_blank">Bhargava2188</a></p> 
-    
+      <p>Linkedin: <b>Bhargava sai Cherukuri</b></p>
     </div>
   )
 }
